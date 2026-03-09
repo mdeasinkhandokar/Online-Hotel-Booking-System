ac585b09f1a87e8c5a8a3fe0c5b2e79135b207df
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2026-03-10 00:52:30 +0600

    Booking entity

--- HotelBooking/src/main/java/com/example/HotelBooking/entities/Booking.java
+++ HotelBooking/src/main/java/com/example/HotelBooking/entities/Booking.java
@@ -7,6 +7,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Entity
 @Data
 @Table(name = "bookings")
@@ -19,7 +23,28 @@ public class Booking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name="user_id")
     private User user;
+
+    @ManyToOne
+    @JoinColumn(name="room_id")
+    private Room room;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    private LocalDate checkInDate;
+    private LocalDate chectOutDate;
+
+
+    private BigDecimal totalPrice;
+    private String bookingReference;
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus bookingStatus;
+
 
 
 
