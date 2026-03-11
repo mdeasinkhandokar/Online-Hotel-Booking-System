4190fa18f96c58016ca648ce507b3537ec893465
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2026-03-12 01:00:46 +0600

    paymentDTO

---
+++ HotelBooking/src/main/java/com/example/HotelBooking/dtos/PaymentDTO.java
@@ -0,0 +1,46 @@
+package com.example.HotelBooking.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PaymentDTO {
+    private Long id;
+    private BookingDTO booking;
+
+    private String transactionId;
+
+    private BigDecimal amount;
+
+    private PaymentGatway paymentGatway;
+
+    private LocalDateTime paymentDate;
+
+    private PaymentStatus status;
+
+    private String bookingReference;
+    private String failureReason;
+
+    private String approvalLink;
+
+
+
+
+
+
+
+
+
+}
