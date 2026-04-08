fafdf284f4f9d8ab70c81fd56d15476fa5a884bf
mdeasinkhandokar <easinkhandokar2000@gmail.com>
2026-04-08 11:22:24 +0600

    Email Service

---
+++ HotelBooking/src/main/java/com/example/HotelBooking/services/NotificationService.java
@@ -0,0 +1,9 @@
+package com.example.HotelBooking.services;
+
+import com.example.HotelBooking.dtos.NotificationDTO;
+
+public interface NotificationService {
+    void sendEmail(NotificationDTO notificationDTO);
+    void sendSms();
+    void sendWhatsapp();
+}
